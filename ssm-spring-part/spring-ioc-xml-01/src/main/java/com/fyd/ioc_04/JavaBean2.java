e92c2284aaa4a20f637d10332c8eef779b44b632
GanFengYu <patient.fyd@gmail.com>
2024-01-27 21:07:48 +0800

    作用域的单例和多例

---
+++ ssm-spring-part/spring-ioc-xml-01/src/main/java/com/fyd/ioc_04/JavaBean2.java
@@ -0,0 +1,10 @@
+package com.fyd.ioc_04;
+
+/**
+ * Author: patient.fyd@gmail.com
+ * Date: 2024/1/27  21:01
+ * Description:
+ */
+public class JavaBean2 {
+
+}
--- ssm-spring-part/spring-ioc-xml-01/src/test/java/com/fyd/test/SpringIoCTest.java
+++ ssm-spring-part/spring-ioc-xml-01/src/test/java/com/fyd/test/SpringIoCTest.java
@@ -2,6 +2,7 @@ package com.fyd.test;
 
 import com.fyd.ioc_03.A;
 import com.fyd.ioc_03.HappyComponent;
+import com.fyd.ioc_04.JavaBean2;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -61,6 +62,11 @@ public class SpringIoCTest {
         // 1.创建ioc容器就会进行组件对象的实例化
         ClassPathXmlApplicationContext applicationContext
                 = new ClassPathXmlApplicationContext("spring-04.xml");
+
+        JavaBean2 bean1 = applicationContext.getBean(JavaBean2.class);
+        JavaBean2 bean2 = applicationContext.getBean(JavaBean2.class);
+        // 多例下，两个不是同一个对象
+        System.out.println(bean1==bean2);
 
         // 2. 正常结束ioc容器,才会调用destroy方法
         applicationContext.close();
