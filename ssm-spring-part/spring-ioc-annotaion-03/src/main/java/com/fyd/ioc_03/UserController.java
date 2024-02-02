40a2d40c2b9521962c6ef90ce925edccffceb27c
GanFengYu <patient.fyd@gmail.com>
2024-02-02 16:36:25 +0800

    @Qualifier多个对象装配问题

--- ssm-spring-part/spring-ioc-annotaion-03/src/main/java/com/fyd/ioc_03/UserController.java
+++ ssm-spring-part/spring-ioc-annotaion-03/src/main/java/com/fyd/ioc_03/UserController.java
@@ -1,6 +1,7 @@
 package com.fyd.ioc_03;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Controller;
 public class UserController {
 
     @Autowired
+    @Qualifier("userServiceImpl") // 只能和AutoWired注解配合使用，当有多个对象的时候，@Qualifier 注解中指定的名称作为 bean 的id进行匹配
+    // @Resource 等同于上面两个之和，Java提供的注解，还需要导入jakarta.annotation-api依赖
     private UserService userService;
 
     public void show(){
