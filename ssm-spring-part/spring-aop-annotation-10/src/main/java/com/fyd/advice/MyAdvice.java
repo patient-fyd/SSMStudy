c32a792a50ac25c0de1a799f4b6fed2d231ec514
GanFengYu <patient.fyd@gmail.com>
2024-02-03 23:10:05 +0800

    在增强类中获取目标类的信息

---
+++ ssm-spring-part/spring-aop-annotation-10/src/main/java/com/fyd/advice/MyAdvice.java
@@ -0,0 +1,40 @@
+package com.fyd.advice;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+/**
+ * Author: patient.fyd@gmail.com
+ * Date: 2024/2/3  22:56
+ * Description: 定义四个增强方法，获取目标方法的信息，返回值，异常对象
+ */
+@Component
+@Aspect
+public class MyAdvice {
+
+    @Before("execution(* com.fyd..impl.*.*(..))")
+    public void before(JoinPoint joinPoint){
+        // 获取方法属于类的信息
+        String simpleName = joinPoint.getTarget().getClass().getSimpleName();
+        // 获取方法名称
+        String methodName = joinPoint.getSignature().getName();
+        // 获取参数列表
+        Object[] args = joinPoint.getArgs();
+    }
+
+    @AfterReturning(value = "execution(* com.fyd..impl.*.*(..))", returning = "result")
+    public void afterReturning(JoinPoint joinPoint, Object result){
+
+    }
+
+    @After("execution(* com.fyd..impl.*.*(..))")
+    public void after(JoinPoint joinPoint){
+        System.out.println("后置通知");
+    }
+
+    @AfterThrowing(value = "execution(* com.fyd..impl.*.*(..))", throwing = "throwable")
+    public void afterThrowing(JoinPoint joinPoint,Throwable throwable){
+        System.out.println("异常通知");
+    }
+}
