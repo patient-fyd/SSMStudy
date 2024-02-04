00d46c7ac5460406c203a9ac1c88274c45ed37e7
GanFengYu <patient.fyd@gmail.com>
2024-02-04 18:29:12 +0800

    只读模式

--- ssm-spring-part/spring-tx-annotation-11/src/main/java/com/fyd/service/StudentService.java
+++ ssm-spring-part/spring-tx-annotation-11/src/main/java/com/fyd/service/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class StudentService {
     
     @Autowired
@@ -15,12 +16,19 @@ public class StudentService {
      *  @ Transactional
      * 添加事务，方法上是当前方法有事务
      *          类上是类下的所有方法都有事务
+     *   1.只读模式
+     *   只读模式可以提升查询事务的效率！推荐事务只有查询代码，使用只读模式
+     *   一般是在类上添加事务，这时候查询也会被添加事务，这个时候可以给查询添加只读
      */
-    @Transactional
     public void changeInfo(){
         studentDao.updateAgeById(88,1);
         int i = 1/0;
         System.out.println("-----------");
         studentDao.updateNameById("test2",1);
+    }
+
+    @Transactional(readOnly = true)
+    public void getStudentInfo(){
+
     }
 }
