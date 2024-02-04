e80337d6f710a81aa22a9048c635d5210d5f9b5c
GanFengYu <patient.fyd@gmail.com>
2024-02-04 19:43:03 +0800

    事务隔离级别

--- ssm-spring-part/spring-tx-annotation-11/src/main/java/com/fyd/service/StudentService.java
+++ ssm-spring-part/spring-tx-annotation-11/src/main/java/com/fyd/service/StudentService.java
@@ -3,6 +3,7 @@ package com.fyd.service;
 import com.fyd.dao.StudentDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.FileInputStream;
@@ -31,8 +32,14 @@ public class StudentService {
      *      默认情况下，指定发生运行时异常事务才会回滚
      *      我们可以指定Exception都会回滚
      *      rollbackFor = Exception.class
+     *
+     *   4.事务隔离级别(推荐使用第二个)
+     *      1. 读未提交（Read Uncommitted）：事务可以读取未被提交的数据，容易产生脏读、不可重复读和幻读等问题。实现简单但不太安全，一般不用。
+     *      2. 读已提交（Read Committed）：事务只能读取已经提交的数据，可以避免脏读问题，但可能引发不可重复读和幻读。
+     *      3. 可重复读（Repeatable Read）：在一个事务中，相同的查询将返回相同的结果集，不管其他事务对数据做了什么修改。可以避免脏读和不可重复读，但仍有幻读的问题。
+     *      4. 串行化（Serializable）：最高的隔离级别，完全禁止了并发，只允许一个事务执行完毕之后才能执行另一个事务。可以避免以上所有问题，但效率较低，不适用于高并发场景。
      */
-    @Transactional(timeout = 3,rollbackFor = Exception.class)
+    @Transactional(timeout = 3,rollbackFor = Exception.class,isolation = Isolation.READ_COMMITTED)
     public void changeInfo() throws FileNotFoundException {
         studentDao.updateAgeById(99,1);
         new FileInputStream("xxx");
