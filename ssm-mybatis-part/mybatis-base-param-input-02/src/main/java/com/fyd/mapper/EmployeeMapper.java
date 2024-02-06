d70686feefaf0ad6b00c0c5a8486d2355abe77f3
GanFengYu <patient.fyd@gmail.com>
2024-02-06 17:31:35 +0800

    多个简单类型传入

--- ssm-mybatis-part/mybatis-base-param-input-02/src/main/java/com/fyd/mapper/EmployeeMapper.java
+++ ssm-mybatis-part/mybatis-base-param-input-02/src/main/java/com/fyd/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.fyd.mapper;
 
 import com.fyd.pojo.Employee;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -18,4 +19,6 @@ public interface EmployeeMapper {
     List<Employee> queryBySalary(Double salary);
 
     int insertEmp(Employee employee);
+
+    List<Employee> queryByNameAndSalary(@Param("name") String name, @Param("salary") Double salary);
 }
